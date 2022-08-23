2f175e9b80d113802ae8c24025c11562f7679a47
vlad <vlazzle@users.noreply.github.com>
2022-08-22 18:37:41 -0700

    added some green color

--- app/src/main/java/com/alltrails/lunch/ui/theme/Color.kt
+++ app/src/main/java/com/alltrails/lunch/ui/theme/Color.kt
@@ -6,3 +6,6 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+val LightGreen800 = Color(0xff558b2f)
+val AllTrailsGreen = Color(0xff428a13)
